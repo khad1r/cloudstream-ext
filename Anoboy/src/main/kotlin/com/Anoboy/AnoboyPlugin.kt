d28811cce6f83fd9178dd20abee3d3932f022915
Kadir <60246846+khad1r@users.noreply.github.com>
2026-07-16 07:58:55 +0700

    Remove unused context parameter from load method in AnoboyPlugin and MovieboxPlugin

--- Anoboy/src/main/kotlin/com/Anoboy/AnoboyPlugin.kt
+++ Anoboy/src/main/kotlin/com/Anoboy/AnoboyPlugin.kt
@@ -2,11 +2,10 @@ package com.anoboy
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
-import android.content.Context
 
 @CloudstreamPlugin
 class AnoboyPlugin: BasePlugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerMainAPI(Anoboy())
 		registerExtractorAPI(Gofile())
     }
--- Moviebox/src/main/kotlin/com/Moviebox/MovieboxPlugin.kt
+++ Moviebox/src/main/kotlin/com/Moviebox/MovieboxPlugin.kt
@@ -2,11 +2,10 @@ package com.moviebox
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.BasePlugin
-import android.content.Context
 
 @CloudstreamPlugin
 class MovieboxPlugin : BasePlugin() {
-    override fun load(context: Context) {
+    override fun load() {
         registerMainAPI(Moviebox())
     }
 }
