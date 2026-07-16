b3ee3acf94216251db03c233ad4847d8ad97371e
Kadir <60246846+khad1r@users.noreply.github.com>
2026-07-16 08:37:53 +0700

    Update authors list in Anoboy and Moviebox plugins; modify repository name and plugin list URL in repo.json

--- Anoboy/build.gradle.kts
+++ Anoboy/build.gradle.kts
@@ -3,7 +3,7 @@ version = 1
 cloudstream {
     language = "id"
 	description = "Anoboy - Streaming Anime Sub Indo"
-    authors = listOf("Asm0d3usX")
+    authors = listOf("Asm0d3usX", "khad1r")
     status = 1
     tvTypes = listOf(
         "AnimeMovie",
